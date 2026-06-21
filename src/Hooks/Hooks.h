7ee0645171ea841cb7ef2da95be1f5fceb6f4ecc
Vitali Kolebyanov <kolebynov18@gmail.com>
2026-06-21 13:17:25 +0400

    Expand Hooks system to support `Direct` hooks with MinHook integration:
    - Introduce `HookType` enum to handle different hook types (`WriteCall` and `Direct`).
    - Add MinHook-based hook installation for `Direct` hooks.
    - Update `HookDefinition` concept and `Install` function to support new functionality.
    - Extend `HooksLib` with `RendererBegin` hook using `Direct` type.

--- src/Hooks/Hooks.h
+++ src/Hooks/Hooks.h
@@ -1,20 +1,50 @@
 #pragma once
 
 #include "Utils/TraitUtils.h"
+#include "MinHook.h"
 
 namespace Hooks {
+    enum class HookType {
+        WriteCall = 1,
+        Direct,
+    };
+
     template <typename T>
     concept HookDefinition =
         FunctionTraits<typename T::FuncDefinition>::IsFunction &&
         std::same_as<typename FunctionTraits<typename T::FuncDefinition>::ReturnType, void> && requires {
             { T::Id } -> std::same_as<const REL::RelocationID&>;
             { T::Offset } -> std::same_as<const REL::VariantOffset&>;
+            { T::Type } -> std::same_as<const HookType&>;
         };
 
     template <HookDefinition THook>
     REL::Relocation<typename THook::FuncDefinition> Install(typename THook::FuncDefinition* func) {
-        REL::Relocation<> hook(THook::Id, THook::Offset);
-        return REL::Relocation<typename THook::FuncDefinition>(
-            SKSE::GetTrampoline().write_call<5>(hook.address(), func));
+        auto targetAddress = REL::Relocation<>(THook::Id, THook::Offset).address();
+        if constexpr (THook::Type == HookType::WriteCall) {
+            return REL::Relocation<typename THook::FuncDefinition>(SKSE::GetTrampoline().write_call<5>(targetAddress, func));
+        } else if constexpr (THook::Type == HookType::Direct) {
+            auto status = MH_Initialize();
+            if (status != MH_OK && status != MH_ERROR_ALREADY_INITIALIZED) {
+                logger::critical("MinHook init failed");
+                std::terminate();
+            }
+
+            LPVOID origAddress;
+            if (MH_CreateHook(reinterpret_cast<LPVOID>(targetAddress), func, &origAddress) != MH_OK) {
+                logger::critical("Failed to create hook");
+                std::terminate();
+            }
+
+            if (MH_EnableHook(reinterpret_cast<LPVOID>(targetAddress)) != MH_OK) {
+                logger::critical("Failed to enable hook");
+                std::terminate();
+            }
+
+            return REL::Relocation<typename THook::FuncDefinition>(reinterpret_cast<uintptr_t>(origAddress));
+        } else {
+            static_assert(!sizeof(THook::Type), "Unsupported hook type");
+            return 0;
+        }
     }
 }
--- src/Hooks/HooksLib.h
+++ src/Hooks/HooksLib.h
@@ -5,17 +5,27 @@ namespace Hooks {
         using FuncDefinition = void(RE::Main*, float);
         static constexpr auto Id = REL::RelocationID(35551, 36544);
         static constexpr auto Offset = REL::VariantOffset(0x11F, 0x160, 0x11F);
+        static constexpr auto Type = HookType::WriteCall;
     };
 
     struct D3DInitHook {
         using FuncDefinition = void();
         static constexpr auto Id = REL::RelocationID(75595, 77226);
         static constexpr auto Offset = REL::VariantOffset(0x50, 0x2BC, 0x00);  // VR unknown
+        static constexpr auto Type = HookType::WriteCall;
     };
 
     struct D3DPresentHook {
-        using FuncDefinition = void __fastcall(std::uint32_t);
+        using FuncDefinition = void(std::uint32_t);
         static constexpr auto Id = REL::RelocationID(75461, 77246);
         static constexpr auto Offset = REL::VariantOffset(0x9, 0x9, 0x15);
+        static constexpr auto Type = HookType::WriteCall;
+    };
+
+    struct RendererBegin {
+        using FuncDefinition = void(RE::BSGraphics::Renderer*, std::uint32_t);
+        static constexpr auto Id = REL::RelocationID(75460, 77245);
+        static constexpr auto Offset = REL::VariantOffset(0, 0, 0);
+        static constexpr auto Type = HookType::Direct;
     };
 }

c7d716cf7cbd34d2b5684d18d685f71f7a164479
Vitali Kolebyanov <kolebynov18@gmail.com>
2026-06-21 13:17:11 +0400

    Add MinHook dependency for trampoline-based detours:
    - Include MinHook in `vcpkg.json` dependencies.
    - Configure MinHook with `find_package` and link to targets in `ExternalDependencies.cmake`.

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -5,6 +5,9 @@
 include(commonlibsse)
 include(cef)
 
+# MinHook (vcpkg: minhook) provides the minhook::minhook target.
+find_package(minhook CONFIG REQUIRED)
+
 # Helper function to add external directories to a target
 function(add_external_dependencies target_name)
 
@@ -12,4 +15,7 @@ function(add_external_dependencies target_name)
     target_include_directories(${target_name} PRIVATE
         ${CMAKE_SOURCE_DIR}/external/commonlibsse-ng/include
     )
+
+    # Link MinHook for trampoline-based detours
+    target_link_libraries(${target_name} PRIVATE minhook::minhook)
 endfunction()

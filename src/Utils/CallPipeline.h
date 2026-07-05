0f4b7401d1969eab2454172aa456a586bf2f2a57
Vitali Kolebyanov <kolebynov18@gmail.com>
2026-07-06 00:38:45 +0400

    Introduce `CallPipeline` utility for action wrapping and chaining:
    - Add `CallPipeline` template class to support function chaining with custom wrappers.

---
+++ src/Utils/CallPipeline.h
@@ -0,0 +1,58 @@
+﻿#pragma once
+
+#include <type_traits>
+
+template <class TAction, class TNext>
+struct CallPipeline {
+public:
+    using Action = std::decay_t<TAction>;
+    using Next = std::decay_t<TNext>;
+
+    CallPipeline(TAction&& action, TNext&& next) :
+        _action(std::forward<TAction>(action)), _next(std::forward<TNext>(next)) {
+    }
+
+    template <class TWrap>
+    auto Wrap(TWrap&& wrap) && {
+        return CallPipeline<TWrap, CallPipeline&&>(std::forward<TWrap>(wrap), std::move(*this));
+    }
+
+    template <class... TArgs>
+    requires (std::is_invocable_v<Action&, Next&, TArgs...>)
+    auto operator()(TArgs... args) {
+        return std::invoke(_action, _next, std::forward<TArgs>(args)...);
+    }
+
+private:
+    Action _action;
+    Next _next;
+};
+
+template <class TAction>
+struct CallPipelineStart {
+public:
+    using Action = std::decay_t<TAction>;
+
+    CallPipelineStart(TAction&& action) :
+        _action(std::forward<TAction>(action)) {
+    }
+
+    template <class TWrap>
+    auto Wrap(TWrap&& wrap) && {
+        return CallPipeline<TWrap, Action&&>(std::forward<TWrap>(wrap), std::move(_action));
+    }
+
+    template <class... TArgs>
+    requires (std::is_invocable_v<Action&, TArgs...>)
+    auto operator()(TArgs... args) {
+        return std::invoke(_action, std::forward<TArgs>(args)...);
+    }
+
+private:
+    Action _action;
+};
+
+template <class TAction>
+CallPipelineStart<TAction> StartCallPipeline(TAction&& action) {
+    return CallPipelineStart<TAction>(std::forward<TAction>(action));
+}
